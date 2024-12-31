f47de4c4e04d8b532b65f65bdbc6edb8599c48a5
Tharnath <itstharnath@gmail.com>
2024-12-30 21:01:29 -0700

    Moved cmakelist.txt -> CoreRTOS.cmake

--- CoreRTOS.cmake
+++ CoreRTOS.cmake
@@ -2,10 +2,10 @@ cmake_minimum_required(VERSION 3.22)
 
 project(CoreRTOS VERSION 1.0 LANGUAGES C)
 
-include_directories(${CMAKE_CURRENT_SOURCE_DIR}/include)
+include_directories(${CMAKE_CURRENT_LIST_DIR}/include)
 
 set(SOURCES
-    ${CMAKE_CURRENT_SOURCE_DIR}/src/scheduler.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/scheduler.c
 )
 
 add_library(CoreRTOS STATIC ${SOURCES})
@@ -13,5 +13,5 @@ add_library(CoreRTOS STATIC ${SOURCES})
 
 target_include_directories(CoreRTOS
     PUBLIC
-    ${CMAKE_CURRENT_SOURCE_DIR}/include
+    ${CMAKE_CURRENT_LIST_DIR}/include
 )
